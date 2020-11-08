0764d11f0406dc39b0585c40c60d756c34ec91d9
jerry861200 <jerry861200@gmail.com>
2020-11-08 18:04:24 +0800

    build a parallax scrolling

---
+++ src/components/BackgroundPic.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="parallax">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.parallax {
+  /* The image used */
+  background-image: url("https://images.pexels.com/photos/1261728/pexels-photo-1261728.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+
+  /* Set a specific height */
+  min-height: 800px;
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: right;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Title
+          </th>
+          <th class="text-left">
+            Description
+          </th>
+          <th class="text-left">
+            URL
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="repo in githubRepo"
+          :key="repo.title"
+        >
+          <td>{{ repo.title }}</td>
+          <td>{{ repo.description }}</td>
+          <td><a :href="repo.url" target="_blank">{{ repo.url }}</a></td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      githubRepo:[
+        {
+          title:'Firetodo',
+          description:'A todoList app that displays CRUD function with firebase database',
+          url:'https://github.com/jerry861200/fireTodo',
+        },
+        {
+          title:'Mind test questionare',
+          description:'A mind test that collects 2400 feedbacks from Dcard.',
+          url:'https://github.com/jerry861200/Questionnaire',
+        },
+        {
+          title:'Wedding',
+          description:'A story tells about the law benefit of being married',
+          url:'https://github.com/jerry861200/wedding',
+        },
+        {
+          title:'PorkerGame',
+          description:'A todoList app that ',
+          url:'https://github.com/jerry861200/porkerGame',
+        },
+        {
+          title:'.',
+          description:'just nothing',
+          url:'xxx',
+        },
+        {
+          title:'.',
+          description:'just nothing',
+          url:'xxx',
+        },
+        {
+          title:'.',
+          description:'just nothing',
+          url:'xxx',
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
